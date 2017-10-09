ca5831d87c6e65e28f9b7fd14b5d8a9d2ce9fb75
Hubert.Krzepkowski <hubert.krzepkowski@globegroup.pl>
2017-10-09 21:19:36 +0200

    projekt

---
+++ Bluetooth-Terminal-master/app/src/main/java/me/aflak/bluetoothterminal/Chat.java
@@ -0,0 +1,228 @@
+package me.aflak.bluetoothterminal;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import me.aflak.bluetooth.Bluetooth;
+
+public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCallback {
+    private String name;
+    private Bluetooth b;
+    private EditText temp;
+    private Button send;
+    private TextView text;
+    private ScrollView scrollView;
+    private boolean registered=false;
+    private Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+    private String dioda;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        text = (TextView)findViewById(R.id.text);
+        temp = (EditText)findViewById(R.id.temp);
+        send = (Button)findViewById(R.id.send);
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+
+        text.setMovementMethod(new ScrollingMovementMethod());
+        send.setEnabled(false);
+        adapter = ArrayAdapter.createFromResource(this,R.array.dioda, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    dioda= "a";
+                } else if (i == 1) {
+                    dioda="b";
+                } else if (i == 2) {
+                   dioda="c";
+
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        b = new Bluetooth(this);
+        b.enableBluetooth();
+
+        b.setCommunicationCallback(this);
+
+        int pos = getIntent().getExtras().getInt("pos");
+        name = b.getPairedDevices().get(pos).getName();
+
+        Display("Łączenie...");
+        b.connectToDevice(b.getPairedDevices().get(pos));
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = temp.getText().toString();
+                String msg1=dioda;
+                String msg2 ="*";
+                temp.setText("");
+                b.send(msg1);
+                b.send(msg);
+                if(msg.length()<2) {
+                    b.send(msg2);
+                }
+              //  Display("You: "+msg);
+            }
+        });
+
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mReceiver, filter);
+        registered=true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(registered) {
+            unregisterReceiver(mReceiver);
+            registered=false;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.close:
+                b.removeCommunicationCallback();
+                b.disconnect();
+                Intent intent = new Intent(this, Select.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void Display(final String s){
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.append(s + "\n");
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+    @Override
+    public void onConnect(BluetoothDevice device) {
+        Display("Połączono z "+device.getName()+" - "+device.getAddress());
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                send.setEnabled(true);
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnect(BluetoothDevice device, String message) {
+        Display("Rozłączono!");
+        Display("Ponownie łączenie...");
+        b.connectToDevice(device);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        Display(name+": "+message);
+    }
+
+    @Override
+    public void onError(String message) {
+        Display("Error: "+message);
+    }
+
+    @Override
+    public void onConnectError(final BluetoothDevice device, String message) {
+        Display("Error: "+message);
+        Display("Ponowne łączenie za 3 sec.");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.connectToDevice(device);
+                    }
+                }, 2000);
+            }
+        });
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                Intent intent1 = new Intent(Chat.this, Select.class);
+
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered=false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered=false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                }
+            }
+        }
+    };
+}
+
---
+++ main.c
@@ -0,0 +1,185 @@
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_adc.h"
+#include "stm32f4xx_usart.h"
+#include "stm32f4xx_exti.h"
+#include "misc.h"
+#include <stdio.h>
+
+float ADC_Result1, ADC_Result2;
+float temp;
+volatile uint32_t a;
+float C;
+int PL=1;
+int Dioda=2500;
+int Liczba1,Liczba2;
+void USART3_IRQHandler(void)
+{
+	// sprawdzenie flagi zwiazanej z odebraniem danych przez USART
+	if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
+       {
+
+			a=USART3->DR;
+
+
+				switch(a)
+					{
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+					case '*':
+						{
+							if(PL==1)
+							{
+								Liczba1=(int)a-48;
+								PL=0;
+
+
+							}
+							else
+							{  if(a=='*'){
+								C=Liczba1;
+							}else
+							{
+								Liczba2=(int)a-48;
+								C=Liczba1*10+Liczba2;
+
+							}
+							PL=1;}
+
+							break;
+						}
+					case'a':{
+						Dioda=0;
+						break;
+						}
+					case'b':{
+						Dioda=2500;
+						break;
+					}
+					case'c':{
+						Dioda=4095;
+						break;
+
+					}
+						}
+
+					}
+				}
+
+
+
+
+
+int main(void)
+{
+	SystemInit();
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA , ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	ADC_CommonInitTypeDef ADC_CommonInitStructure;
+	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+	ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
+	ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+	ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+	ADC_CommonInit(&ADC_CommonInitStructure);
+
+	ADC_InitTypeDef ADC_InitStructure;
+	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfConversion = 1;
+	ADC_Init(ADC1, &ADC_InitStructure);
+
+	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_NbrOfConversion = 1;
+	ADC_Init(ADC2, &ADC_InitStructure);
+
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_84Cycles);
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_2, 0, ADC_SampleTime_84Cycles);
+
+	ADC_Cmd(ADC1, ENABLE);
+	ADC_Cmd(ADC2, ENABLE);
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
+
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART3, &USART_InitStructure);
+
+	USART_Cmd(USART3, ENABLE);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
+	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	NVIC_EnableIRQ(USART3_IRQn);
+    while(1)
+    {
+
+    	ADC_SoftwareStartConv(ADC1);
+    	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
+    	ADC_Result1 = ADC_GetConversionValue(ADC1);
+    	if(ADC_Result1<Dioda) GPIO_SetBits(GPIOB,GPIO_Pin_1);
+    		else GPIO_ResetBits(GPIOB,GPIO_Pin_1);
+
+    	ADC_SoftwareStartConv(ADC2);
+    	while(ADC_GetFlagStatus(ADC2, ADC_FLAG_EOC) == RESET);
+    	ADC_Result2 = ADC_GetConversionValue(ADC2);
+    	temp=(293*ADC_Result2/4095)-50;
+    	if(temp<C) GPIO_SetBits(GPIOB,GPIO_Pin_0);
+    		else GPIO_ResetBits(GPIOB,GPIO_Pin_0);
+    }
+}
